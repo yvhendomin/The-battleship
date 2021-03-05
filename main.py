c6e3c6bfe46cde748699214a561332a08e40da71
yevhendomin <dyomjke@gmail.com>
2021-03-05 16:30:20 +0200

    class Field() updated

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ CELL = 20 * SIZE_FACTOR
 WIN_SIZE_X = 683 * SIZE_FACTOR
 WIN_SIZE_Y = 384 * SIZE_FACTOR
 TARGET_STEP = CELL
-
+DATA = [(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+        (0, 0, 0, 0, 0, 0, 0, 0, 0, 0)]
 YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -14,26 +23,21 @@ BLUE = (0, 0, 255)
 
 class Field():
     """Class Field"""
-    def __init__(self, player_id, x, y, size, data):
+    def __init__(self, player_id, x, y):
         """initializing attributes:
         player_id - whose player is field,
         start_coord - from which point it will drawn
          """
         self.player_id = player_id
-        self.size = size
-        self.data = [(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-                     (0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-                     (0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-                     (0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-                     (0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-                     (0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-                     (0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-                     (0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-                     (0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-                     (0, 0, 0, 0, 0, 0, 0, 0, 0, 0)]
+        self.data = DATA
         self.length = CELL * 10
         self.x = x
         self.y = y
+        self.serif = None
+        self.serif2 = None
+        self.pos = None
+        self.pos2 = None
+        self.digits = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10')
 
     def printf(self):
         """This method is drawing field on screen"""
@@ -45,33 +49,26 @@ class Field():
         for i in range(self.y, self.y + self.length + TARGET_STEP, TARGET_STEP):
             pygame.draw.line(win, YELLOW, [self.x, i], [self.x + self.length, i], 1)
 
-        """font = pygame.font.SysFont('arial', 21 * SIZE_FACTOR, False, False, )
-        text1 = font.render("A B C D E F G H J K", 1, YELLOW, BLACK)
-        text2 = font.render("1 2 3 4 5 6 7 8 9 10", 1, YELLOW, BLACK)
-        pos = text1.get_rect(center=(248, 270))
-        pos2 = text2.get_rect(center=(740, 270))"""
 
+        font = pygame.font.SysFont('arial', 22 * SIZE_FACTOR, False, False)
+        self.serif = font.render('A B C D F G J K L M', 1, YELLOW, BLACK)
+        self.pos = self.serif.get_rect(center=(self.length // 2 + self.x, self.y - CELL // 1.5))
+        win.blit(self.serif, self.pos)
+        for i in range(1, 11):
+            self.serif2 = font.render(self.digits[i], 1, YELLOW, BLACK)
+            self.pos2 = self.serif2.get_rect(center=(self.x - CELL, self.y + (i * CELL) - CELL))
+            win.blit(self.serif2, self.pos2)
 
 pygame.init()
 win = pygame.display.set_mode((WIN_SIZE_X, WIN_SIZE_Y))
 pygame.display.set_caption("THE BATTLESHIP")
 
-"""font = pygame.font.SysFont('arial', 21 * SIZE_FACTOR, False, False, )
-text1 = font.render("A B C D E F G H J K", 1, YELLOW, BLACK)
-text2 = font.render("1 2 3 4 5 6 7 8 9 10", 1, YELLOW, BLACK)
-pos = text1.get_rect(center=(248, 270))
-pos2 = text2.get_rect(center=(740, 270))"""
 
-field1_pos_x = 50
-fields_pos_y = 300
-field2_pos_x = 540
-field_width = 400
-field_height = 400
 target_pos_x = 50
 target_pos_y = 300
 target_size = 40
 
-field1 = Field(1, 50, 100, 1, 1)
+field1 = Field(1, 50, 100)
 
 
 run = True
