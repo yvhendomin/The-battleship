39922cebc11280572b2f5bf022e7fdd7815b47e7
yevhendomin <dyomjke@gmail.com>
2021-03-05 20:04:05 +0200

    Field print ok, Began work with target

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import classes as cl
 
 SIZE_FACTOR = 1
 CELL = 20 * SIZE_FACTOR
@@ -19,7 +20,9 @@ YELLOW = (255, 255, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
-
+X = CELL * 3
+Y = CELL * 4
+TARGET_COORD = (1, 1)
 
 class Field():
     """Class Field"""
@@ -37,12 +40,15 @@ class Field():
         self.serif2 = None
         self.pos = None
         self.pos2 = None
-        self.digits = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10')
+        self.digits = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10')
+
 
     def printf(self):
         """This method is drawing field on screen"""
         pygame.draw.rect(win, BLUE, (self.x, self.y, self.length, self.length))
-        pygame.draw.rect(win, YELLOW, (self.x, self.y, TARGET_STEP, TARGET_STEP))
+
+        if TARGET_COORD != (0, 0):
+            pygame.draw.rect(win, YELLOW, (TARGET_COORD[0] * CELL, TARGET_COORD[1] * CELL, TARGET_STEP, TARGET_STEP))
 
         for i in range(self.x, self.x + self.length + TARGET_STEP, TARGET_STEP):
             pygame.draw.line(win, YELLOW, [i, self.y], [i, self.y + self.length], 1)
@@ -51,24 +57,22 @@ class Field():
 
 
         font = pygame.font.SysFont('arial', 22 * SIZE_FACTOR, False, False)
-        self.serif = font.render('A B C D F G J K L M', 1, YELLOW, BLACK)
+        self.serif = font.render('A B C D F G J K L M', 0, YELLOW, BLACK)
         self.pos = self.serif.get_rect(center=(self.length // 2 + self.x, self.y - CELL // 1.5))
         win.blit(self.serif, self.pos)
-        for i in range(1, 11):
-            self.serif2 = font.render(self.digits[i], 1, YELLOW, BLACK)
-            self.pos2 = self.serif2.get_rect(center=(self.x - CELL, self.y + (i * CELL) - CELL))
+        for i in range(0, 10):
+            self.serif2 = font.render(self.digits[i], 0, YELLOW, BLACK)
+            self.pos2 = self.serif2.get_rect(center=(self.x - CELL, self.y + CELL // 2 + i * CELL))
             win.blit(self.serif2, self.pos2)
+
 
 pygame.init()
 win = pygame.display.set_mode((WIN_SIZE_X, WIN_SIZE_Y))
 pygame.display.set_caption("THE BATTLESHIP")
 
 
-target_pos_x = 50
-target_pos_y = 300
-target_size = 40
-
-field1 = Field(1, 50, 100)
+field1 = Field(1, X, Y)
+field2 = Field(2, X + 14 * CELL, Y)
 
 
 run = True
@@ -79,17 +83,17 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-
-    """keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and target_pos_x > 50:
-        target_pos_x -= TARGET_STEP
-    elif keys[pygame.K_RIGHT] and target_pos_x < 410:
-        target_pos_x += TARGET_STEP
-    elif keys[pygame.K_DOWN] and target_pos_y < 660:
-        target_pos_y += TARGET_STEP
-    elif keys[pygame.K_UP] and target_pos_y > 300:
-        target_pos_y -= TARGET_STEP"""
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and TARGET_COORD[0] > 0:
+        TARGET_COORD[0] -= 1
+    elif keys[pygame.K_RIGHT] and TARGET_COORD[0] < 10:
+        TARGET_COORD[0] += 1
+    elif keys[pygame.K_DOWN] and TARGET_COORD[1] < 10:
+        TARGET_COORD[1] += 1
+    elif keys[pygame.K_UP] and TARGET_COORD[1] > 0:
+        TARGET_COORD -= 1
     field1.printf()
+    field2.printf()
     """win.fill((0, 0, 0))"""
 
     """win.blit(text1, pos)"""
